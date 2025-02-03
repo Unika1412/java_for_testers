e056af3d13ff3a78bf4dcec203b47432696402d2
AnyaSemina <hhhanya@mail.ru>
2025-02-03 12:13:40 +0500

    структуризация кода по классам и пакетам

--- sandbox/src/main/java/ru/vs/geometry/figures/Rectangle.java
+++ sandbox/src/main/java/ru/vs/geometry/figures/Rectangle.java
@@ -2,7 +2,8 @@ package ru.vs.geometry.figures;
 
 public class Rectangle {
     public static void printSquareRectangleArea(double a, double b) {
-        System.out.println("Area of a rectangle with side " + a + " and side " + b + " = " + RectangleArea(a,b));
+        var text = String.format("Area of a rectangle with side %f and side %f = %f", a, b, RectangleArea(a,b));
+        System.out.println(text);
     }
 
     private static double RectangleArea(double a, double b) {
--- sandbox/src/main/java/ru/vs/geometry/figures/Square.java
+++ sandbox/src/main/java/ru/vs/geometry/figures/Square.java
@@ -2,7 +2,8 @@ package ru.vs.geometry.figures;
 
 public class Square {
     public static void printSquareArea(double a){
-        System.out.println("Area of a square with side " + a + " = " + SquareArea(a));
+        String text = String.format("Area of a square with side %f = %f", a, SquareArea(a));
+        System.out.println(text);
     }
 
     private static double SquareArea(double a) {
